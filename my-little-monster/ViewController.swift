b1d45bb58d9b9349091e14c0a5a80b678166bcf6
Jonathan Wood <jonathankimballwood@gmail.com>
2015-09-10 18:29:25 -0600

    initial commit

---
+++ my-little-monster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-little-monster
+//
+//  Created by Jonathan Wood on 9/10/15.
+//  Copyright © 2015 Jonathan Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
