bdbc3cf2f9852dbdb0d296c7e4a4d55db42f5611
Jonathan Wood <jonathankimballwood@gmail.com>
2015-09-10 18:07:05 -0600

    initial commit

---
+++ sub-classed-viewTests/sub_classed_viewTests.swift
@@ -0,0 +1,36 @@
+//
+//  sub_classed_viewTests.swift
+//  sub-classed-viewTests
+//
+//  Created by Jonathan Wood on 9/10/15.
+//  Copyright © 2015 Jonathan Wood. All rights reserved.
+//
+
+import XCTest
+@testable import sub_classed_view
+
+class sub_classed_viewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
